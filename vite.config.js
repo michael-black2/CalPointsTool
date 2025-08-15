9b292e2915c335431e774e33ea8cd251fecfff36
michael-black2 <59457439+michael-black2@users.noreply.github.com>
2025-08-15 12:31:18 -0700

    Point to CalPointsTool
    - Updated vite.config.js base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  base: '/CalPointsTool/',   // <-- IMPORTANT for Pages under /<repo>/
 })
