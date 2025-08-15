0885a8d9041727f189688614dfccafe404bd7753
michael-black2 <59457439+michael-black2@users.noreply.github.com>
2025-08-15 12:39:58 -0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// USE ONE of these two lines ↓↓↓
+// const base = '/CalPointsTool/'  // when using the GitHub Pages URL
+const base = '/'                   // when using a custom domain
+
 export default defineConfig({
+  base,
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-  base: '/CalPointsTool/',   // <-- IMPORTANT for Pages under /<repo>/
 })
